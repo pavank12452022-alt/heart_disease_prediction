1251ea4e7f2e30c1814ab3514c1906cff9537ec4
pavank12452022-alt <pavank12452022@gmail.com>
2026-09-02 18:56:55 +0530

    Update Streamlit app

--- heart_app.py
+++ heart_app.py
@@ -7,7 +7,7 @@ model = joblib.load("heart_model.pkl")
 scaler = joblib.load("scaler.pkl")
 expected_columns = joblib.load("features.pkl")
 
-st.title("Heart Stroke Prediction by akarsh")
+st.title("Heart Stroke Prediction by pavan")
 st.markdown("Provide the following details to check your heart stroke risk:")
 
 # Collect user input
